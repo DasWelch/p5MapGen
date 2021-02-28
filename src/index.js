85c7e248a0429ae40524e1eb947c0f11240c899b
DasWelch <35710104+DasWelch@users.noreply.github.com>
2021-02-28 14:46:36 -0500

    have generate button working

--- src/index.js
+++ src/index.js
@@ -21,14 +21,14 @@ let sketch = function (p5) {
     makeCanvas(w, h, p5, canvasParentRef);
     generateMap(w, h, p5, canvasParentRef);
   };
-  p5.mouseClicked = () =>{
+  // p5.mouseClicked = () =>{
     
-    if(p5.canvas !== 'undefined'){
+  //   if(p5.canvas !== 'undefined'){
       
-      generateMap(100, 100, p5, canvasParentRef);
-      console.log(p5.canvas !== 'undefined')
-    }
-  }
+  //     generateMap(100, 100, p5, canvasParentRef);
+  //     console.log(p5.canvas !== 'undefined')
+  //   }
+  // }
 };
 
 function makeCanvas(w, h, p5, canvasParentRef) {
@@ -69,7 +69,7 @@ console.log(p5);
 myp5 = new p5(sketch);
 
 function generateNewMap(){
-  generateMap(100, 100, p5, canvasParentRef)
+  generateMap(100, 100, myp5, canvasParentRef)
 }
 
 // setTimeout(() => {
