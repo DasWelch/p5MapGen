2d28a8edfae662e6a1a4d03a039d7d93770eb56e
DasWelch <35710104+DasWelch@users.noreply.github.com>
2021-02-27 19:51:03 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,18 +9,24 @@
 // lacunarity directly controls the number of small features
 // persistance is like coherance, how much do these small features affect the cohesion of the whole map
 
-const canvasParentRef = document.getElementById("sketch-holder")
+const canvasParentRef = document.getElementById("sketch-holder");
+w = 500;
+h = 500;
 
 let sketch = function (p5) {
-  
-  p5.setup = function() {
-    generateMap(p5,canvasParentRef);
-  }
+  p5.setup = function () {
+    makeCanvas(w, h, p5, canvasParentRef);
+    generateMap(w, h, p5, canvasParentRef);
+  };
 };
 
-function generateMap(p5, canvasParentRef) {
-  let wsize = 1000;
-  let hsize = 500;
+function makeCanvas(w, h, p5, canvasParentRef) {
+  p5.createCanvas(w, h).parent(canvasParentRef);
+}
+
+function generateMap(w, h, p5, canvasParentRef) {
+  let wsize = p5.width;
+  let hsize = p5.height;
   let seed = -1;
   let offset = p5.createVector(0.0, 0.0);
   let scale = 64;
@@ -28,7 +34,6 @@ function generateMap(p5, canvasParentRef) {
   let lacunarity = 1;
   let persistance = 1;
 
-  p5.createCanvas(wsize, hsize).parent(canvasParentRef);
   p5.background(255);
   let mapGen = new MapGenerator(
     p5,
@@ -43,6 +48,5 @@ function generateMap(p5, canvasParentRef) {
   ); // higher the scale the greateer the zoom in?
   mapGen.GenerateMap();
 }
-
 
 let myp5 = new p5(sketch);
